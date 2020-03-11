eebe1e87570df30190c1a2eb64cb24cc488ece66
sooheon <sooheon.k@gmail.com>
2020-03-12 08:49:21 +0900

    fish update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,7 +8,7 @@ set fish_greeting
 set -x fish_user_paths ~/bin /usr/local/bin /usr/local/sbin ~/.local/bin $fish_user_paths
 
 ## add mkl libs to dyld path
-set -x DYLD_LIBRARY_PATH /opt/intel/mkl/lib:/opt/intel/lib
+set -x DYLD_LIBRARY_PATH /opt/intel/mkl/lib /opt/intel/lib
 
 ## select toolchain for swift
 set -x PATH /Library/Developer/Toolchains/swift-latest/usr/bin $PATH
@@ -22,8 +22,10 @@ alias gcc=gcc-9
 # ASDF
 source /usr/local/opt/asdf/asdf.fish
 
+
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /Users/skim/.asdf/installs/python/miniconda3-4.3.30/bin/conda "shell.fish" "hook" $argv | source
+eval /Users/sooheon/.asdf/installs/python/miniconda3-4.3.30/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
+set -g fish_user_paths "/usr/local/opt/ncurses/bin" $fish_user_paths
